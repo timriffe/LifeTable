1a890c2c4910fda8b67c379600a7c0a968dfd435
triffe <tim.riffe@gmail.com>
2013-11-20 21:44:14 -0800

    LT major changes. simplified ages

--- LifeTable/R/LT.R
+++ LifeTable/R/LT.R
@@ -7,18 +7,16 @@
 #' @param Dx numeric vector of death counts by age.
 #' @param Mx numeric vector of central death rates (assumed in the function to be the lifetable m(x)), calculated as deaths/exposure. 
 #' @param ages optional, default = \code{"auto"}. If \code{"auto"}, the function tried to guess whether you have single-age data or 5-year abridged age groups. These are used in the lifetable labels, and do not enter into calculations. Otherwise, the user can specify a vector (character or numeric).
-#' @param type either \code{"single-age"} (default) or \code{"abridged"}. These are the only two options. Five year abridged age groups presumes that you have age intervals of 1,4,5,5,5,5.... Send requests to the author if this isn't flexible enough.
 #' @param axmethod either \code{"keyfitz"}, \code{"schoen"}, \code{"preston"} or \code{"midpoint"}. Default = \code{"keyfitz"}, although this is not recommended for abridged ages. See comparisons in \code{\link{axEstimate}} examples. The user can also supply a numeric vector of a(x) values here (e.g. from a different estimation procedure or from a different population).
 #' @param sex either \code{"male"} or \code{"female"} (default). It is only necessary to specify this if \code{"preston"} is the \code{axmethod}. It does not affect any other lifetable calculations.
 #' @param mxsmooth logical, default = \code{TRUE}. Should the mx vector be smoothed? If \code{TRUE} and both \code{Nx} and \code{Dx} vectors are supplied (the ideal case), smoothing is done using the function \code{Mort1Dsmooth()} from Giancarlo Camarda's \code{MortalitySmooth} package. In this case, \code{Dx} values are smoothed using \code{log(Nx)} as an offset, and all other items are the function defaults. If \code{Mx} is provided instead of \code{Nx} and \code{Dx} a loess smoother is used, \code{loess}, with span set to .15 for single age data and .4 for 5-year abridged data. If these smoothing procedures are not satisfactory, the user may wish to pre-process the Mx estimate and specify \code{mxsmooth = FALSE}, or else leave it rough.
 #' @param axsmooth logical, default = \code{TRUE}. Ignored if \code{mxsmooth = TRUE}. Should the a(x) values be calculated from a smoothed M(x) series? In this case, the M(x) series is smoothed within the \code{axEstimate()} function for a(x) estimation, but the smoothed M(x) function that was used is not returned. In general, it is better to smooth the M(x) function prior to putting it in this function, because the loess smoother used here has no weights or offset. If this is not possible, loess M(x) smoothing still produces more consistent and less erratic a(x) estimates. If \code{mxsmooth = FALSE} and \code{axsmooth = TRUE}, the Mx series is only smoothed for use in a(x) estimation, and does not affect any other lifetable calculations that are dependent on Mx. 
-#' @param n numeric vector of interval widths or \code{"auto"} (default). 
 #' @param radix The lifetable starting population at age 0, l(0). default = 1. Other common values are 1000 and 100000, although any value may be given.
 #' @param verbose logical, default = \code{TRUE}. Should informative but possibly annoying messages be returned when the function does something that you might want to know about?
 #' 
 #' @details Either \code{Nx} must be specified along with \code{Dx}, *or* \code{Mx} must be specified directly. If smoothing is used, it is better to specify both \code{Nx} and \code{Dx}, since the \code{Nx} vector can be used as an offset in the \code{MortalitySmooth} smoother.
 #' 
-#' @return a list
+#' @return a list is invisibly returned
 #' \itemize{
 #'   \item \code{LT} A \code{data.frame} with 11 columns and as many rows as you have ages. The columns are "Age" (character age labels, i.e. with "+"), "ages" (numeric), \code{"mx", "ax", "qx", "px", "lx", "dx", "Lx", "Tx", "ex"}.
 #' All the individual components of the lifetable can be called and are unrounded when individually called. Some additional values are also available:
@@ -108,8 +106,8 @@
 #' 
 
 LT <-
-function(Nx, Dx, Mx, ages = "auto", type = "single-age", axmethod = "keyfitz", sex = "female", 
-		mxsmooth = TRUE, axsmooth = TRUE, n = "auto", radix = 1, verbose = TRUE){
+function(Nx=NULL, Dx=NULL, Mx = Dx/Nx, ages = 0:(length(Mx)-1), axmethod = "midpoint", sex = "female", 
+		mxsmooth = TRUE, axsmooth = TRUE, radix = 1, verbose = TRUE){
 	# the verbose function:
 	Verb <- function(v, x){
 		if (v) {
@@ -117,30 +115,21 @@ function(Nx, Dx, Mx, ages = "auto", type = "single-age", axmethod = "keyfitz", s
 		}
 	}
 	# first a series of checks, messages and imputations to make sure the given Dx&Nx *or* Mx values are viable
-	if (missing(Mx)){
+   
+	if (length(Mx) == 0){
 		# two checks that will stop the function
 		# 1) in absence of Mx, need both Dx and Nx
-		if (missing(Nx) | missing(Dx)){ 
+		if (is.null(Nx) | is.null(Dx)){ 
 			Verb(verbose,"you're missing the key arguments")
 			stop("either specify Mx directly, or else specify both Nx and Dx.")
 		}
 		# 2) both Nx and Dx must be of equal length
-		if (length(Nx) != length(Dx)){
-			Nxl <- length(Nx)
-			Dxl <- length(Dx)
-			Verb(verbose,"Nx and Dx lengths not equal\nplease examine and adjust these vectors")
-			stop(paste("Nx length =", Nxl, ", Dx length =", Dxl))
-		}
-		# try to coerce to numeric vectors
-		Nx <- as.numeric(c(unlist(Nx))) 
-		Dx <- as.numeric(c(unlist(Dx)))
-		
+        stopifnot(length(Nx) != length(Dx))
+	
 		# safety to avoid zeros in the denominator. will make Mx of 1 in those cases
 		if (any(Nx == 0)) {
-			Verb(verbose, "there was at least one 0 in your Nx vector.")
-			Verb(verbose, (paste("index value(S)=", which(Nx == 0))))
-			Verb(verbose, "the function imputed the corresponding Dx value into the Nx 0s\nwhich makes an Mx of 1 in those slots, and it was thus able to continue.\nif this was just the last Nx value(s), then it makes no difference.\nLook over the Nx vector to verify that there are no other 0s that could be more problematic")
-			Nx[Nx == 0] <- Dx[Nx == 0]
+			Verb(verbose, "there was at least one 0 in your Nx vector, imputed with corresponding Dx.")
+            Nx[Nx == 0] <- Dx[Nx == 0]
 		}
 		Mx <- Dx / Nx
 	}
@@ -149,55 +138,28 @@ function(Nx, Dx, Mx, ages = "auto", type = "single-age", axmethod = "keyfitz", s
 	# we want to be able to accept 0s...
 	
 	# N is just used for counting, to save space
-	N <- length(Mx) # 
-	
-	# identify single-age or abridged table
-	types 	    <- c("single-age", "abridged", "other")
-	typesmenu 	<- c("single-age (0,1,2,3....110+)", "abridged (0,1-4,5-9...)", "other?")
-	if ((! type %in% types) | missing(type)) {
-		type 	<- types[menu(typesmenu, graphics = TRUE, title = "pick relevant lifetable type")]
-	}
-	if (type == "other") {
-		stop("sorry, I only have the first 2 kinds of age-categories implemented so far\nplease send suggestions to tim.riffe@gmail.com")
-	}
-	
-	# assign interval widths, including the open interval
-    if (is.character(n)){
-        if (n == "auto" & type == "abridged"){
-            Widths      <- c(1, 4, rep(5, N - 2))
-        }
-        if (n == "auto" & type == "single-age"){
-            Widths      <- rep(1, N)
-        }    
+	N                   <- length(Mx) # 
+	Widths              <- c(1, diff(ages))
+    
+    # define character for Age in formatted lifetable
+	if (all(Widths == 1)){
+        Age <- as.character(ages)
+        Age[N] <- paste0(Age[N],"+")
+    } else {
+        Age <- c(0,paste(ages[2:(N - 1)], ages[2:(N - 1)] + Widths - 1,sep="-"),paste0(Age[N],"+"))
     }
-	
-	if (is.numeric(n)){	
-		Widths 		<- n
-		Verb(verbose, "used user-supplied widths. If there is an error, be sure this vector is of the same length as Nx,Dx")
-	}
-	
-	if (ages == "auto" & type == "abridged") {
-		Age 			<- c(0, paste(c(1, seq(from = 5, by = 5, length = (N - 3))), "-",
-						c(4, seq(from = 9, by = 5, length = (N - 3))), sep = ""), paste0(((N - 2) * 5), "+"))
-	}
-	if (ages == "auto" & type == "single-age") {
-		Age 			<- c(seq(0, N - 2), paste0(N - 1, "+"))
-	}
-	# redefine term ages for later use.
-	if (ages == "auto"){
-		ages 			<- cumsum(Widths) - Widths
-	}
+    
 	ages.mids.pre 		<- ages + Widths / 2
 	ages.mids.pre[1] 	<- .1
 	
-	if(!missing(Nx) & !missing(Dx) & mxsmooth){
+	if(!is.null(Nx) & !is.null(Dx) & mxsmooth){
 		# Giancarlo's package. I recommend either supplying an already-smoothed Mx vector (for complete control)
 		# or else supplying Dx and Nx and setting mxsmooth to TRUE.
 		fitBIC 			<- Mort1Dsmooth(x = ages.mids.pre, y = Dx, offset = log(Nx))
 		Mx[2:N] 		<- (fitted(fitBIC) / Nx)[2:N]
 	}
 	
-	if(missing(Nx) & missing(Dx) & mxsmooth){
+	if(is.null(Nx) & is.null(Dx) & mxsmooth){
 		Verb(verbose,"mxsmooth was specified as TRUE, but since Mx was supplied directly, \nthere are no implicit weights (Nx). Function used a loess smoother \nto smooth out the Mx, but please be wary.")
 		span 			<- ifelse(N > 30, .15, .4)
 		logMx 			<- log(Mx)
@@ -208,39 +170,40 @@ function(Nx, Dx, Mx, ages = "auto", type = "single-age", axmethod = "keyfitz", s
 
 	# Assume that the lifetable mx is equal to the central death rate.
 	mx                  <- Mx
-	
-	# these later 3 imputations should not be needed, but are there just in case.
+	# these later 2 imputations should not be needed, but are there just in case.
 	mx[is.na(mx)] 		<- 0
 	mx[is.infinite(mx)] <- 0
 	
 	# we don't want 0s in m(x) for calculating a(x), because a(x) (except midpoint) is iterative
 	# and we'd erroneously bring down neighboring age's ax values with zeros.
 	# for later calulations, the zeros are taken 'as-is'
+    Ind0 <- NULL
 	if (length(axmethod) == 1 & min(mx) == 0){
-		Ind0    <- mx == 0
+		Ind0        <- mx == 0
 		
 		Verb(verbose, paste("\n\n*there were some ages (", ages[Ind0],
                         ") with no mortality.\nValues are imputed for calculating a(x), but the zeros are kept for the rest of the lifetable.\n"))
-		span    <- ifelse(N > 30,.15,.4)
-		logMx   <- log(Mx)
+		span        <- ifelse(N > 30,.15,.4)
+		logMx       <- log(Mx)
 		logMx[is.infinite(logMx)] <- NA
-		Imp     <- exp(predict(loess(logMx ~ ages.mids.pre, span = span, control = loess.control(surface = "interpolate")), newdata = ages))[Ind0]
+		Imp         <- exp(predict(loess(logMx ~ ages.mids.pre, span = span, control = loess.control(surface = "interpolate")), newdata = ages))[Ind0]
 		if (any(is.na(Imp))){
-			Imp <- exp(spline(ages.mids.pre, logMx, xmin = 0, xmax = max(ages))$y[Ind0])
+			Imp     <- exp(spline(ages.mids.pre, logMx, xmin = 0, xmax = max(ages))$y[Ind0])
 		}
-		mx[Ind0] <- Imp
+		mx[Ind0]    <- Imp
 	}
 	
 	if (length(axmethod) == 1){
 		if (axmethod %in% c("keyfitz", "schoen", "midpoint", "preston")){
 			# here, the ax iteration is externalized to axEstimate()
 			if (mxsmooth){
+                Verb(verbose,"\nif mxsmooth = TRUE, then we don't smooth ax\n")
 				axsmooth <- FALSE
 			}
 			ax <- axEstimate(Mx = mx, n = Widths, axsmooth = axsmooth, method = axmethod, sex = sex, verbose = verbose)
 		}
-		if (axmethod == "keyfitz" & type == "abridged"){
-			Verb(verbose, "It appears you have an abridged lifetable, but have specified the keyfitz method of ax estimation.\nBe aware that this method presumes equal age intervals, as Preston et. al. (2001)\n warn on page 45. Consider using a different method or else specifying your own ax vector.\n Function continued nonetheless.")
+		if (axmethod == "keyfitz" & !all(Widths == 1)){
+			Verb(verbose, "\nIt appears you have an abridged lifetable, but have specified the keyfitz method of ax estimation.\nBe aware that this method presumes equal age intervals, as Preston et. al. (2001)\n warn on page 45. Consider using a different method or else specifying your own ax vector.\n Function continued nonetheless.")
 		}
 	}
 	
@@ -254,51 +217,56 @@ function(Nx, Dx, Mx, ages = "auto", type = "single-age", axmethod = "keyfitz", s
 	}
 	
 	# if zeros were imputed for ax estimation, then we put them back for the rest of the calculations
-	if (exists("Ind0")){
-		mx[Ind0] <- 0
+	if (is.null(Ind0)){
+		mx[Ind0]        <- 0
 	}
-	
-	qx 			    <- (Widths * mx) / (1 + (Widths - ax) * mx)
-	qx[N] 		    <- 1
+    
+	# now start the lifetable columns:
+	qx 			        <- (Widths * mx) / (1 + (Widths - ax) * mx)
+	qx[N] 		        <- 1
 	
 	# can't have qx > 1, so we impute 1s where necessary: hopefully only at the penultimate, as the case may be
-	qx[qx > 1] 	    <- 1
-	px 			    <- 1 - qx
+	qx[qx > 1] 	        <- 1
+	px 			        <- 1 - qx
 	
-    lx              <- c(radix, radix * cumprod(px))[1:N]
+    lx                  <- c(radix, radix * cumprod(px))[1:N]
+	dx 			        <- -diff(lx)
+	dx[N] 		        <- lx[N]
 	
-	dx 			    <- -diff(lx)
-	dx[N] 		    <- lx[N]
-	
-	Lx[1:(N - 1)] 	<- Widths[1:(N - 1)] * lx[2:N] + ax[1:(N - 1)] * dx[1:(N - 1)]
-	Lx[N] 		    <- lx[N] / mx[N]
+	Lx 	                <- c(Widths[1:(N - 1)] * lx[2:N] + ax[1:(N - 1)] * dx[1:(N - 1)],lx[N] / mx[N])
 	Lx[is.infinite(Lx)] <- 1
-	Lx[is.na(Lx)] 	<- 0
+	Lx[is.na(Lx)] 	    <- 0
 	
-	Tx 			    <- rev(cumsum(rev(Lx)))
-	ex 			    <- Tx / lx
-	ex[N]           <- ifelse(mx[N] == 0, ax[N], {1 / mx[N]})
+	Tx 			        <- rev(cumsum(rev(Lx)))
+	ex 			        <- Tx / lx
+	ex[N]               <- ifelse(mx[N] == 0, ax[N], {1 / mx[N]})
 	
 	# Sx is the pertinent output for projection matrices
-    Sx 			    <- vector(length = N)
-	Sx[1:(N - 1)] 	<- (Lx[2:N] / Widths[2:N]) / (Lx[1:(N-1)] / Widths[1:(N - 1)])
-	Sx[N]       	<- Tx[N] / Tx[(N - 1)]
+    Sx 			        <- vector(length = N)
+	Sx[1:(N - 1)] 	    <- (Lx[2:N] / Widths[2:N]) / (Lx[1:(N-1)] / Widths[1:(N - 1)])
+	Sx[N]       	    <- Tx[N] / Tx[(N - 1)]
 	# two not-very satisfying, and possibly redundant, substitutions:
-	Sx[Lx == 0]   	<- 0
-	Sx[is.na(Sx)] 	<- 0
+	Sx[Lx == 0]   	    <- 0
+	Sx[is.na(Sx)] 	    <- 0
 	
 	LT <- data.frame(cbind("Age" = Age, "ages" = ages, "mx" = round(mx, 5), "ax" = round(ax, digits = 2),
 					"qx" = round(qx, 5), "px" = round(px, 5), "lx" = round(lx, 5),
 					"dx" = round(dx, 5), "Lx" = round(Lx, 5), "Tx" = round(Tx, 5), "ex" = round(ex, 3)))
 	# both LT as well as the individual pieces (not rounded) can be called
-	return(list(LT = LT, 
-                    Age = Age, 
-                    ages = ages, 
-                    mx = mx, 
-                    ax = ax, 
-                    qx = qx, 
-                    lx = lx, 
-                    dx = dx, Lx = Lx, Tx = Tx, ex = ex, Sx = Sx, Widths = Widths))
+	out <- list(LT = LT, 
+            Age = Age, 
+            ages = ages, 
+            mx = mx, 
+            ax = ax, 
+            qx = qx, 
+            lx = lx, 
+            dx = dx, 
+            Lx = Lx, 
+            Tx = Tx, 
+            ex = ex, 
+            Sx = Sx, 
+            Widths = Widths)
+    invisible(out)
 }
 
 #' @name LifeTable
--- LifeTable/Rother/build.R
+++ LifeTable/Rother/build.R
@@ -3,7 +3,7 @@
 ###############################################################################
 
 library(devtools)
-load_all("/home/triffe/git/LifeTable/LifeTable")
+load_all("/home/triffe/git/LifeTable/LifeTable",TRUE)
 document("/home/triffe/git/LifeTable/LifeTable")
 
 library(tools)
@@ -18,5 +18,33 @@ sapply(Rdfiles, function(xxx, parent.path, package.path){
             #go to folder
         },parent.path = parent.path, package.path = package.path)
 system(paste0("cd ", parent.path, " \n git checkout gh-pages \n git add help * \n git commit -m 'update help'"))
+
+
+# ---------------------------------------------
+library(devtools)
+install_github("LifeTable", subdir = "LifeTable", username = "timriffe")
+
+library(LifeTable)
+data(UKRmales1965)
+head(UKRmales1965)
+Nx <- UKRmales1965[, 3]
+Dx <- UKRmales1965[, 2]
+LT(Nx, Dx, ages = 0:110, axsmooth = TRUE)$e0est
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
